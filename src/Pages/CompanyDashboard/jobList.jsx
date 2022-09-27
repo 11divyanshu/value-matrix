8566b739ffcae0d5a2a7bc404dda2d8035a3326d
17Oranges!Azam <azamuddin95mohd@gmail.com>
2022-09-27 10:55:45 +0530

    updated jobs count

--- src/Pages/CompanyDashboard/jobList.jsx
+++ src/Pages/CompanyDashboard/jobList.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import JobCard from "../../Components/Dashbaord/JobCard.jsx";
 import { listJobs } from "../../service/api.js";
 import { CSVLink } from "react-csv";
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form } from "formik";
 import { FilterCompany } from "../../service/api.js";
 import Loader from "../../assets/images/loader.gif";
 import Avatar from "../../assets/images/UserAvatar.png";
-import { BsFillBookmarkFill } from 'react-icons/bs'
-import { HiOutlineUser } from 'react-icons/hi'
+import { BsFillBookmarkFill } from "react-icons/bs";
+import { HiOutlineUser } from "react-icons/hi";
 import { Popover, Transition } from "@headlessui/react";
 
 const JobList = () => {
@@ -15,10 +15,10 @@ const JobList = () => {
   const [loader, setLoader] = React.useState(false);
   const [user, setUser] = React.useState(null);
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     let user = JSON.parse(localStorage.getItem("user"));
     setUser(user);
-  },[])
+  }, []);
   const headerso = [
     { label: "job_id", key: "_id" },
     { label: "job_title", key: "jobTitle" },
@@ -31,26 +31,20 @@ const JobList = () => {
     { label: "valid_till", key: "validTill" },
     { label: "hiring_organization", key: "hiringOrganization" },
     { label: "basic_salary", key: "basicSalary" },
-
-  ]
+  ];
 
   const csvReport = {
     filename: "jobs.csv",
     headers: headerso,
     data: jobs,
-  }
-
-
-
-
+  };
 
   React.useEffect(() => {
     const getData = async () => {
-
       let c_id = JSON.parse(localStorage.getItem("user"));
       console.log(c_id);
       let res = await listJobs(c_id._id);
-      console.log(res)
+      console.log(res);
       if (res && res.data) {
         setJobs(res.data.jobs);
         console.log(res.data.jobs);
@@ -59,11 +53,7 @@ const JobList = () => {
 
         // save to localStorage
         localStorage.setItem("jobsdetails", jsonObj);
-
-
       }
-
-
     };
     getData();
   }, []);
@@ -86,45 +76,46 @@ const JobList = () => {
       // setLoader(true);
       setLoader(false);
       setTimeout(() => {
-
         setJobs(res.data.jobs);
       }, 1000);
       // setFilter(res.data.jobs);
 
-      console.log(jobs)
+      console.log(jobs);
       const jsonObj = JSON.stringify(arr);
 
       // save to localStorage
       localStorage.setItem("jobsdetails", jsonObj);
-
-
-
     } else {
-      console.log("no response")
+      console.log("no response");
     }
-  }
-
-
-
-
-
-
+  };
 
   return (
-
     <div className="bg-slate-100">
-      <div className="flex mx-5 mt-3" style={{ justifyContent: 'space-between' }}>
+      <div
+        className="flex mx-5 mt-3"
+        style={{ justifyContent: "space-between" }}
+      >
         {/* <p className="text-2xl mx-3 font-semibold pl-3 mt-5">All Jobs</p> */}
-        <p className="text-sm flex my-5 mx-5 font-semibold">Hey {user && user.firstName ? user.firstName :"Company" } - <p className="text-gray-400 px-2"> here's what's happening today!</p></p>
+        <p className="text-sm flex my-5 mx-5 font-semibold">
+          Hey {user && user.firstName ? user.firstName : "Company"} -{" "}
+          <p className="text-gray-400 px-2"> here's what's happening today!</p>
+        </p>
 
         <div className="py-3">
-
-
-          <p className="text-gray-900 text-s mb-2 mx-5 text-right text-blue"><CSVLink {...csvReport}><button class=" p-3 w-10vw rounded-md text-white" style={{ backgroundColor: "#034488" }}>Download CSV</button></CSVLink></p>
+          <p className="text-gray-900 text-s mb-2 mx-5 text-right text-blue">
+            <CSVLink {...csvReport}>
+              <button
+                class=" p-3 w-10vw rounded-md text-white"
+                style={{ backgroundColor: "#034488" }}
+              >
+                Download CSV
+              </button>
+            </CSVLink>
+          </p>
         </div>
       </div>
-      <div className="p-4 w-full md:flex mx-auto" >
-
+      <div className="p-4 w-full md:flex mx-auto">
         {/* <div className="md:w-1/4 sm:w-full  mt-5 h-3/5 shadow-lg rounded-lg">
 
 
@@ -180,11 +171,16 @@ const JobList = () => {
 
         </div> */}
         <div className=" md:w-3/4 md:mx-5">
-
-          {loader ? <p>...Loading</p> :
+          {loader ? (
+            <p>...Loading</p>
+          ) : (
             <>
               <div className="flex justify-between w-full bg-white">
-                <div className="  py-4 px-5" style={{ borderRadius: "6px 6px 0 0" }}><p className="text-gray-900 w-full font-bold">Posted Jobs</p>
+                <div
+                  className="  py-4 px-5"
+                  style={{ borderRadius: "6px 6px 0 0" }}
+                >
+                  <p className="text-gray-900 w-full font-bold">Posted Jobs</p>
                   {/* <p className="text-gray-400 w-full font-semibold">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> */}
                 </div>
 
@@ -192,87 +188,91 @@ const JobList = () => {
               </div>
 
               <div className="w-full">
-
-
-                {jobs && (
+                {jobs &&
                   jobs.map((job) => {
-                    return (
-                      <JobCard job={job} />
-                    )
-                  })
-                )}
+                    return <JobCard job={job} />;
+                  })}
               </div>
             </>
-          }
+          )}
         </div>
 
         <div className="md:w-1/4 my-3">
           <div className="shadow-lg  py-5  bg-white  justify-around  px-5 bg-white">
             <p className="text-xl mx-auto text-gray-700 font-bold  flex">
-              <p className="p-1"><BsFillBookmarkFill /></p>
+              <p className="p-1">
+                <BsFillBookmarkFill />
+              </p>
               <p className=" mx-2  text-sm ">My Items</p>
             </p>
             <div className="border-b border-gray-600 flex justify-between my-4 py-4">
-              <p className="font-bold text-xs">Posted Jobs</p><p className="text-gray-400 font-semibold text-xs">04</p>
+              <p className="font-bold text-xs">Posted Jobs</p>
+              <p className="text-gray-400 font-semibold text-xs">
+                {jobs.length > 0 ? jobs.length : 0}
+              </p>
             </div>
             <div className="border-b border-gray-600 flex justify-between my-4 py-4">
-              <p className="font-bold text-xs">My Learnings</p><p className="text-gray-400 font-semibold text-xs">06</p>
+              <p className="font-bold text-xs">My Learnings</p>
+              <p className="text-gray-400 font-semibold text-xs">06</p>
             </div>
             <div className=" border-gray-600 flex justify-between mt-4 pt-4">
-              <p className="font-bold text-xs">Save Posts</p><p className="text-gray-400 font-semibold text-xs">01</p>
+              <p className="font-bold text-xs">Save Posts</p>
+              <p className="text-gray-400 font-semibold text-xs">01</p>
             </div>
           </div>
 
           <div className="shadow-lg sm:w-full rounded-lg   py-5  bg-white  justify-around my-4 h-auto  px-4 bg-white">
             <p className="text-xl px-2 mx-auto text-gray-700 font-bold  flex">
-
               {/* <div className=" px-6 mx-2 py-1 ml-5 text-center" ><AiOutlineUnorderedList/></div> */}
 
-
-              <p className=" mx-2  text-sm ">Support</p><p className=""><HiOutlineUser /></p>
+              <p className=" mx-2  text-sm ">Support</p>
+              <p className="">
+                <HiOutlineUser />
+              </p>
             </p>
 
             <div className="flex justify-between text-xs py-4 px-3">
-              <div><p>Open 0/5</p></div>
-              <div><p>Working 0/5</p></div>
-              <div><p>Closed 0/5</p></div>
+              <div>
+                <p>Open 0/5</p>
+              </div>
+              <div>
+                <p>Working 0/5</p>
+              </div>
+              <div>
+                <p>Closed 0/5</p>
+              </div>
             </div>
-            <div className="flex px-2 vertical-align-middle" >
+            <div className="flex px-2 vertical-align-middle">
               <img src={Avatar} className="rounded-full w-12 h-12 m-3" />
-              <div> <p className="py-2 text-sm">Cameron Williamson</p>
+              <div>
+                {" "}
+                <p className="py-2 text-sm">Cameron Williamson</p>
                 <p className="text-gray-400 text-xs">Product Designer</p>
               </div>
             </div>
 
-            <div className="flex px-2 vertical-align-middle" >
+            <div className="flex px-2 vertical-align-middle">
               <img src={Avatar} className="rounded-full w-12 h-12 m-3" />
-              <div> <p className="py-2 text-sm">Brookyln Simmons</p>
+              <div>
+                {" "}
+                <p className="py-2 text-sm">Brookyln Simmons</p>
                 <p className="text-gray-400 text-xs">Software Engineer</p>
               </div>
             </div>
 
-
-            <div className="flex px-2 vertical-align-middle" >
+            <div className="flex px-2 vertical-align-middle">
               <img src={Avatar} className="rounded-full w-12 h-12 m-3" />
-              <div> <p className="py-2 text-sm">Leslie Alexander</p>
+              <div>
+                {" "}
+                <p className="py-2 text-sm">Leslie Alexander</p>
                 <p className="text-gray-400 text-xs">Project Manager</p>
               </div>
             </div>
 
-
-
-
-
             {/* <button className="bg-blue-600 rounded-lg px-6 mx-2 my-3 py-2 text-xs text-gray-900 font-semibold">View List</button> */}
-
-
           </div>
-
         </div>
-
-
       </div>
-
     </div>
   );
 };
