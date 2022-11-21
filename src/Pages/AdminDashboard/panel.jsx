67a3da2bd8d80710b6c15cae2acbf35e7b29d8c9
Satyam Sharma <satyamsharma1725@gmail.com>
2022-11-21 11:14:42 +0530

    admin component

--- src/Pages/AdminDashboard/panel.jsx
+++ src/Pages/AdminDashboard/panel.jsx
@@ -19,6 +19,8 @@ import Board from "../../assets/images/board.svg";
 import Graph from "../../assets/images/graph.png";
 import LGraph from "../../assets/images/lgraph.png";
 
+import RecentPeople from "./RecentPeople.jsx"
+
 import { BsThreeDots } from "react-icons/bs";
 import logo from "../../assets/images/logo.png";
 import swal from "sweetalert";
@@ -508,47 +510,7 @@ const Panel = () => {
               <SessionCard />
             </div>
 
-            <div className="shadow-lg sm:w-full rounded-lg md:w-full lg:w-full py-10 my-8 h-auto mx-5 bg-white px-4">
-              <p className="text-xl px-4 mx-auto text-gray-700 font-bold  flex">
-                {/* <div className=" px-6 mx-2 py-1 ml-5 text-center" ><AiOutlineUnorderedList/></div> */}
-
-                <p className="px-6 mx-2  text-xl">Recent People</p>
-              </p>
-              <div className="flex my-4 px-5 vertical-align-middle">
-                <img src={Avatar} className="rounded-full w-12 h-12 m-3" />
-                <div>
-                  {" "}
-                  <p className="py-2 font-md">Rahul Pandey</p>
-                  <p className="text-gray-400 text-sm">
-                    Web Developer , UI/UX Designer
-                  </p>
-                </div>
-              </div>
-
-              <div className="flex my-4 px-5 vertical-align-middle">
-                <img src={Avatar} className="rounded-full w-12 h-12 m-3" />
-                <div>
-                  {" "}
-                  <p className="py-2 font-md">Rahul Pandey</p>
-                  <p className="text-gray-400 text-sm">
-                    Web Developer , UI/UX Designer
-                  </p>
-                </div>
-              </div>
-
-              <div className="flex my-4 px-5 vertical-align-middle">
-                <img src={Avatar} className="rounded-full w-12 h-12 m-3" />
-                <div>
-                  {" "}
-                  <p className="py-2 font-md">Rahul Pandey</p>
-                  <p className="text-gray-400 text-sm">
-                    Web Developer , UI/UX Designer
-                  </p>
-                </div>
-              </div>
-
-              {/* <button className="bg-blue-600 rounded-lg px-6 mx-2 my-3 py-2 text-xs text-gray-900 font-semibold">View List</button> */}
-            </div>
+            <RecentPeople/>
           </div>
         </div>
       </div>
